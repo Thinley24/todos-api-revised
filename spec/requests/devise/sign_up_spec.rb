f5c93181276da74ad06340c2a82a04f5fe36ae84
Thinley24 <thinleyjigme705@gmail.com>
2023-07-20 15:17:47 +0600

    More expectations included

--- spec/requests/devise/sign_up_spec.rb
+++ spec/requests/devise/sign_up_spec.rb
@@ -10,9 +10,8 @@ RSpec.describe 'User Authentication Sign up', type: :request do
       it 'returns a JSON response with a status of 201 and a success message' do
         post '/users', params: { user: user_attributes }
         expect(response).to have_http_status(201)
-        expect(json_response)
         expect(json_response).to have_key('id')
-        expect(json_response['email']).to eq(user.email)
+        expect(json_response['email']).to eq(user_attributes[:email])
       end
     end
   end
